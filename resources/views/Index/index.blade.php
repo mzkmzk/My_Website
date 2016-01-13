c7481f6be8205c9ca6e13f354ce71e4ea5d14130
Mai ZhiKun <maizk@haplox.com>
2016-01-13 21:02:41 +0800

    初版首页

---
+++ resources/views/Index/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>K</title>
+        <link href="css/K.css" rel="stylesheet">
+    </head>
+    <body>
+    <!--<img style="width: 100% ;position: absolute" src="../public/Image/Index/index.webp">-->
+    <header>
+        <div class="menu">
+            <button class="bt">Menu</button>
+        </div>
+    </header>
+    <section id="content">
+        <div class="container text-center">
+            <div class="col-sm-4 "><button>GitBook</button></div>
+            <div class="col-sm-4"><button>Github</button></div>
+            <div class="col-sm-4"><button>New_Blog</button></div>
+            <div class="col-sm-4"><button>Toutiao</button></div>
+            <div class="col-sm-4"><button>Processon</button></div>
+            <div class="col-sm-4"><button>Old_Blog</button></div>
+
+        </div>
+    </section>
+    </body>
+</html>
