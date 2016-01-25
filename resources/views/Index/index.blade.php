8f4ef14053ffc5d781f16a9caa78cdfd74b3fd65
Mai ZhiKun <maizk@haplox.com>
2016-01-25 14:23:01 +0800

    首页小屏幕修改

--- resources/views/Index/index.blade.php
+++ resources/views/Index/index.blade.php
@@ -6,8 +6,7 @@
         <link href="css/K.css" rel="stylesheet">
 
     </head>
-    <body  class="">
-    <!--<img style="width: 100% ;position: absolute" src="../public/Image/Index/index.webp">-->
+    <body  >
     <header>
         <div class="menu">
             <button class="bt">Menu</button>
@@ -22,14 +21,14 @@
     <section id="content" >
         <div  class="container text-center show-button animated slideOutDown"  >
             <div class="row">
-                <a target="_blank" href="https://github.com/mzkmzk"><div class="col-sm-offset-4  col-sm-4"><button>Github</button></div></a>
+                <div class="col-sm-offset-4  col-sm-4"><a target="_blank" href="https://github.com/mzkmzk"><button>Github</button></a></div>
             </div>
-            <a target="_blank" href="http://toutiao.io/u/139248"><div class="col-sm-4"><button>Toutiao</button></div></a>
-            <a target="_blank" href="https://www.gitbook.com/@mzkmzk"><div class="col-sm-4 "><button>GitBook</button></div></a>
-            <a target="_blank" href="http://mzkmzk.github.io/"><div class="col-sm-4"><button>New_Blog</button></div></a>
-            <a target="_blank" href="http://i.youku.com/404MZK"><div class="col-sm-4"><button>YouKu</button></div></a>
-            <a target="_blank" href="https://www.processon.com/u/55f7b3bee4b0e7f16fb0d5ce/profile?tab=diagrams"><div class="col-sm-4"><button>Processon</button></div></a>
-            <a target="_blank" href="http://blog.163.com/mz_kk/"><div class="col-sm-4"><button>Old_Blog</button></div></a>
+           <div class="col-sm-4"> <a target="_blank" href="http://toutiao.io/u/139248"><button>Toutiao</button></a></div>
+            <div class="col-sm-4 "><a target="_blank" href="https://www.gitbook.com/@mzkmzk"><button>GitBook</button></a></div>
+            <div class="col-sm-4"><a target="_blank" href="http://mzkmzk.github.io/"><button>New_Blog</button></a></div>
+            <div class="col-sm-4"><a target="_blank" href="http://i.youku.com/404MZK"><button>YouKu</button></a></div>
+            <div class="col-sm-4"><a target="_blank" href="https://www.processon.com/u/55f7b3bee4b0e7f16fb0d5ce/profile?tab=diagrams"><button>Processon</button></a></div>
+            <div class="col-sm-4"><a target="_blank" href="http://blog.163.com/mz_kk/"><button>Old_Blog</button></a></div>
 
 
 
