dd6e3f8ff9c9b90cc2e89fe653e69d1750b6d4bc
Mai ZhiKun <maizk@haplox.com>
2016-05-25 11:14:15 +0800

    not ignore recase false

---
+++ resources/views/index/index.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <title>K</title>
+        <link href="css/K.css" rel="stylesheet">
+
+    </head>
+    <body  >
+    <header>
+        <div class="menu">
+            <button class="bt">Menu</button>
+        </div>
+    </header>
+    <section id="background">
+        <div class="background-image " >
+
+
+        </div>
+    </section>
+    <section id="content" >
+        <div  class="container text-center show-button animated slideOutDown"  >
+            <div class="row">
+                <div class="col-sm-offset-4  col-sm-4"><a target="_blank" href="https://github.com/mzkmzk"><button>Github</button></a></div>
+            </div>
+           <div class="col-sm-4"> <a target="_blank" href="http://toutiao.io/u/139248"><button>Toutiao</button></a></div>
+            <div class="col-sm-4 "><a target="_blank" href="https://www.gitbook.com/@mzkmzk"><button>GitBook</button></a></div>
+            <div class="col-sm-4"><a target="_blank" href="http://mzkmzk.github.io/"><button>New_Blog</button></a></div>
+            <div class="col-sm-4"><a target="_blank" href="http://i.youku.com/404MZK"><button>YouKu</button></a></div>
+            <div class="col-sm-4"><a target="_blank" href="https://www.processon.com/u/55f7b3bee4b0e7f16fb0d5ce/profile?tab=diagrams"><button>Processon</button></a></div>
+            <div class="col-sm-4"><a target="_blank" href="http://blog.163.com/mz_kk/"><button>Old_Blog</button></a></div>
+
+
+
+        </div>
+    </section>
+    <script src="js/K.js"></script>
+    </body>
+</html>
